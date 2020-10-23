1b964f7041db3fbe52062a584805ab73e90740a5
celsoRG <celso.re.go@hotmail.com>
2020-10-22 23:15:01 -0500

    Realice ejercicios de la clase react desde cero de Edteam hasta el episodio JsX a profundidad

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import "./styles/styles.scss"
+import React, {Fragment} from 'react';
+//import logo from './logo.svg';
+//import './App.css'; 
+import "./styles/styles.scss";
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <>
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt= "baneer" src="https://ed.team/cursos/ingles-avanzado"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Curso EDteam</p>
+        <p> Tu futuro te esta esperando</p>
+        <a href="#" className="button">Suscribirse</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+<div className="ed-grid m-grid-3">   
+ <Curso/>
+ <Curso/>
+ <Curso/>
+</div>
+</>
+)
 
 export default App;
+
+//toda eiqueta debe cerrarse
+//todos los componenres deben devolver un solo elemento padre
+//apoyarse de los fragment si necesito devolver doos elementos <Fragment></Fragment> o <> </>
+//img siempre se cierran
+//Class pasa a ser className
+// for pasa a ser htmlFor
+
+
---
+++ src/Nombre.js
@@ -0,0 +1,8 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css'; 
+//import "./styles/styles.scss"
+
+const Nombre = () => <h1>Att: Celso</h1>
+
+export default Nombre;
